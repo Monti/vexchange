d8d8508b1dd5d86f7c96ef04dbfbb97805a3c8e3
kennethashley <ken.u.diggitt@gmail.com>
2019-03-03 15:22:00 -0500

    Add ReactGA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import ReactGA from 'react-ga';
 import ProviderModal from './components/ProviderModal';
 import WebFont from 'webfontloader';
 import './i18n';
@@ -9,6 +10,14 @@ import store from './store';
 
 import 'antd/dist/antd.css';
 import './index.scss';
+
+if (process.env.NODE_ENV === 'development') {
+  // ReactGA.initialize('UA-128182339-02');
+} else {
+  ReactGA.initialize('UA-135474754-1');
+}
+
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 WebFont.load({
   google: {
